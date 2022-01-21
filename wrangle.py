d65f12b10a1db739b525803a9d17453d446bec1e
Lori Ainslie <lori.ainslie18@gmail.com>
2022-01-21 12:07:58 -0600

    updating with current progress

--- wrangle.py
+++ wrangle.py
@@ -6,6 +6,16 @@ import seaborn as sns
 
 import yfinance as yf
 
+
+from datetime import datetime
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt, ExponentialSmoothing, SimpleExpSmoothing
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARIMA
+
 # create function to pull in data
 
 def get_crypto_data():
@@ -78,3 +88,103 @@ def prep_crypto_data():
     cryptocurrencies = split_crypto_data(nem, holo, nem_hr, holo_hr)
 
     return cryptocurrencies
+
+
+# # compute rmse to evaluate model
+# def evaluate(validate_set, yhat_df, target_var):
+#     rmse = round(sqrt(mean_squared_error(validate_set[target_var], yhat_df[target_var])), 5)
+#     return rmse
+
+
+# # plot original and predicted values
+# def plot_and_eval(train_set, validate_set, yhat_df, target_var):
+#     plt.plot(train_set[target_var], label = 'Train', linewidth = 1)
+#     plt.plot(validate_set[target_var], label = 'Validate', linewidth = 1)
+#     plt.plot(yhat_df[target_var], label = 'Prediction', linewidth = 1)
+#     plt.title(target_var)
+#     plt.legend()
+#     rmse = evaluate(validate_set, yhat_df, target_var)
+#     print(f'{target_var} -- RMSE: {rmse}')
+#     plt.show()
+    
+
+# # append evaluations to a df for comparison
+# def append_eval_df(model_type, validate_set, target_var):
+#     rmse = evaluate(validate_set, target_var)
+#     d = {'model_type': [model_type], 'target_var': [target_var], 'rmse': [rmse]}
+#     d = pd.DataFrame(d)
+#     return eval_df.append(d, ignore_index = True)
+
+
+# compute rmse to evaluate model
+def evaluate(validate_set, yhat_df):
+    rmse = round(sqrt(mean_squared_error(validate_set['Close'], yhat_df['Close'])), 5)
+    return rmse
+
+# plot original and predicted values
+def plot_and_eval(train_set, validate_set, yhat_df, model):
+    plt.plot(train_set['Close'], label = 'Train', linewidth = 1)
+    plt.plot(validate_set['Close'], label = 'Validate', linewidth = 1)
+    plt.plot(yhat_df['Close'], label = 'Prediction', linewidth = 1)
+    plt.title('Actual Closing Price vs ' + model)
+    plt.legend()
+    plt.xticks(rotation=45)
+    rmse = evaluate(validate_set, yhat_df)
+    print(f'{model} RMSE: {rmse}')
+    plt.show()
+
+
+def append_eval_df(validate_set, yhat_df, model):
+    rmse = evaluate(validate_set, yhat_df)
+    d = pd.DataFrame({'model': [model], 'rmse': [rmse]})
+    return eval_df.append(d, ignore_index = True)
+
+
+def predict_evaluate_baseline(train_set, validate_set):
+    # predict using mean of train close data
+    model = 'Baseline Prediction'
+    close = round(train_set['Close'].mean(), 5)
+    yhat_df = pd.DataFrame({'Close': [close]}, index = validate_set.index)
+    print(f'{model}  = {close}')
+
+    plot_and_eval(train_set, validate_set, yhat_df, model)
+
+
+def predict_evaluate_1yr_mavg(train_set, validate_set):
+    # predict using mean of train close data
+    model = '1-Year Moving Avg'
+    close = round(train_set.Close.rolling(365).mean().iloc[-1], 5)
+    yhat_df = pd.DataFrame({'Close': [close]}, index = validate_set.index)
+    print(f'{model}  = {close}')
+
+    plot_and_eval(train_set, validate_set, yhat_df, model)
+
+
+def predict_evaluate_holts_linear(train_set, validate_set):
+    # predict using mean of train close data
+    model = 'Holt\'s Linear Trend'
+    close = Holt(train_set.Close, exponential = False)
+    close = close.fit(smoothing_level = .2, smoothing_slope = .9, optimized = True)
+    yhat_df = pd.DataFrame(round(close.predict(start = validate_set.index[0], end = validate_set.index[-1]), 5), columns=['Close'])
+
+    plot_and_eval(train_set, validate_set, yhat_df, model)
+
+
+def predict_evaluate_arima(train_set, validate_set):
+    # predict using mean of train close data
+    model = 'ARIMA'
+    close = ARIMA(train_set.Close, order=(5,0,0))
+    close = close.fit()
+    yhat_df = pd.DataFrame(round(close.predict(start = validate_set.index[0], end = validate_set.index[-1]), 5), columns=['Close'])
+
+    plot_and_eval(train_set, validate_set, yhat_df, model)
+
+
+# # create the model
+# model = ARIMA(nem_train.Close, order=(5,0,0))
+# # fit the model
+# model = model.fit()
+# # use the model to predict
+# yhat_items = model.predict(start = nem_validate.index[0], end = nem_validate.index[-1])
+# # add items to yhat df
+# yhat_df[col] = round(yhat_items, 5)
